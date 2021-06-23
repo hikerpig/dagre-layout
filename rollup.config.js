796b9aed3432827fa8a0792791063db910d352d5
hikerpig <hikerpigwinnie@gmail.com>
2021-06-23 20:59:54 +0800

    Add minified output

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 import rollupTypescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { terser } from "rollup-plugin-terser"
 import pkg from './package.json'
 import tsconfig from './tsconfig.json'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
   input: 'src/index.ts',
@@ -13,12 +16,18 @@ const config = {
       name: 'dagre',
     },
     {
-      // file: pkg.module,
       format: 'esm',
       dir: 'dist',
-      entryFileNames: 'dagre-layout.esm.js',
+      entryFileNames: 'dagre-layout.js',
     },
-  ],
+  ].concat(isProd ?[
+    {
+      file: pkg.umd.replace(/\.js$/, '.min.js'),
+      format: 'umd',
+      name: 'dagre',
+      plugins: [terser()]
+    },
+  ]: []),
   plugins: [
     commonjs(),
     nodeResolve(),
