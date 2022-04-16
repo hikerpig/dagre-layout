acca3ae308a1dfcf1c5dc24a7c918b07fd3c34cd
hikerpig <hikerpigwinnie@gmail.com>
2022-04-16 20:09:07 +0800

    optimize: consider margint and marginb during `doPositionY`

--- src/position/index.ts
+++ src/position/index.ts
@@ -1,6 +1,6 @@
 import * as _ from '../util-lodash'
 
-import util from '../util'
+import util, { getNum } from '../util'
 import { positionX } from './bk'
 import { DNode, DagreGraph } from '../type'
 
@@ -17,18 +17,29 @@ function doPositionY(g: DagreGraph) {
   const rankSep = g.graph().ranksep
   let prevY = 0
   _.forEach(layering, function (layer) {
+    let yOffset1 = 0 // upper offset due to parent margint
+    let yOffset2 = 0 // bottom offset due to parent marginb
     const maxHeight = _.max(
       _.map(layer, function (v) {
         if (v) {
           const node: DNode = g.node(v)
-          return node.height + (node.margint || 0) + (node.marginb || 0)
+          const parent = g.parent(v) && g.node(g.parent(v))
+          if (parent) {
+            yOffset1 = Math.max(getNum(parent.margint), yOffset1)
+            yOffset2 = Math.max(getNum(parent.marginb), yOffset2)
+          }
+          return (
+            node.height +
+            getNum(node.margint) +
+            getNum(node.marginb)
+          )
         }
       })
     )
     _.forEach(layer, function (v) {
-      if (v) g.node(v).y = prevY + maxHeight / 2
+      if (v) g.node(v).y = prevY + yOffset1 + maxHeight / 2
     })
-    prevY += maxHeight + rankSep
+    prevY += maxHeight + rankSep + yOffset1 + yOffset2
   })
 }
 
